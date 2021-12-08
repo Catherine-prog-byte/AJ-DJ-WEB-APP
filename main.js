27b07974666d135a8da393b3774e85ebe94de8be
Catherine-prog-byte <77283675+Catherine-prog-byte@users.noreply.github.com>
2021-12-08 19:48:02 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,16 +1,44 @@
 song="";
+leftWristX=0;
+leftWristY=0;
+rightWristX=0;
+rightWristY=0;
 function setup() {
     canvas=createCanvas(600,550);
     canvas.center();
     video=createCapture(VIDEO);
     video.hide();
+
+    poseNet = ml5.poseNet(video , modelLoaded);
+    poseNet.on('pose', gotPoses);
 }
+
+function modelLoaded() {
+    console.log("poseNet is Initialized");
+}
+ 
 function draw() {
     image(video,0,0,600,550);
 }
+
 function preload() {
     song=loadSound("music.mp3");
 }
+
 function play() {
     song.play();
+    song.setVolume(0.5);
+    song.rate(1);
+}
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log(results);
+        leftWristX=results[0].pose.leftWrist.x;
+        leftWristY=results[0].pose.leftWrist.y;
+        console.log("leftWristX = " + leftWristX+",leftWristY ="+ leftWristY);
+        
+        rightWristX=results[0].pose.rightWrist.x;
+        rightWristY=results[0].pose.rightWrist.y;
+        console.log("rightWritsX = " + rightWristX+",rightWristY ="+ rightWristY);
+    }
 }
