1dedfff39042e1b38b5a6067f8706ec4bb6a7ef8
Catherine-prog-byte <77283675+Catherine-prog-byte@users.noreply.github.com>
2021-12-10 19:36:05 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ leftWristX=0;
 leftWristY=0;
 rightWristX=0;
 rightWristY=0;
+scoreleftWrist=0;
 function setup() {
     canvas=createCanvas(600,550);
     canvas.center();
@@ -19,6 +20,19 @@ function modelLoaded() {
  
 function draw() {
     image(video,0,0,600,550);
+
+    fill("#E18D7B");
+    stroke("#E18D7B");
+
+   if (scoreleftWrist >  0.2)   
+ {
+    circle(leftWristX,leftWristY,20);
+    InNumberleftWristY = Number(leftWristY);
+    remove_decimals = floor(InNumberleftWristY);
+    volume= remove_decimals/500;
+    document.getElementById("volume").innerHTML = "Volume =" +volume;
+    song.setVolume(volume);
+  }
 }
 
 function preload() {
@@ -33,6 +47,8 @@ function play() {
 function gotPoses(results) {
     if (results.length > 0) {
         console.log(results);
+        scoreleftWrist = results[0].pose.keypoints[9].score;
+        console.log("scoreLeftWrist =" + scoreleftWrist);
         leftWristX=results[0].pose.leftWrist.x;
         leftWristY=results[0].pose.leftWrist.y;
         console.log("leftWristX = " + leftWristX+",leftWristY ="+ leftWristY);
